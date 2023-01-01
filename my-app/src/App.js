623e31de2cadacf816ff2ecc088ec8fa2d0d1e50
MaroMohamedSalah <91625271+MaroMohamedSalah@users.noreply.github.com>
2023-01-02 00:12:32 +0200

    Add font

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,15 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
 
-function App() {}
+function App() {
+	return (
+		<div className="container">
+			<Nav />
+		</div>
+	);
+}
 
 export default App;
---
+++ my-app/src/components/Nav.js
@@ -0,0 +1,14 @@
+const Nav = () => {
+	return (
+		<nav className="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-between">
+			<h1>
+				Love Calculator{" "}
+				<span className="icon">
+					<i class="fa-solid fa-house"></i>
+				</span>
+			</h1>
+		</nav>
+	);
+};
+
+export default Nav;
