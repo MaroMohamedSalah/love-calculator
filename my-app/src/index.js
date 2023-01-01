300d93d21f584862babc1656c86ffd7aeea6dcdd
MaroMohamedSalah <91625271+MaroMohamedSalah@users.noreply.github.com>
2023-01-01 23:31:39 +0200

    Add react router

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
