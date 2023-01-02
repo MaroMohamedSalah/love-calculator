d453b6f217449edcf182d886d523a6b74e6d1671
MaroMohamedSalah <91625271+MaroMohamedSalah@users.noreply.github.com>
2023-01-03 00:26:14 +0200

    implement Home page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/js/bootstrap.min.js";
 import { Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
+import Result from "./pages/Result";
 
 function App() {
 	return (
@@ -11,7 +12,11 @@ function App() {
 			<Nav />
 			<Routes>
 				<Route path="/" element={<Home />} />
-				<Route path="records" element={<h1>Hi</h1>} />
+				<Route
+					path="records"
+					element={<h1 className="text-center">i'm still working in it.</h1>}
+				/>
+				<Route path="result" element={<Result />} />
 			</Routes>
 		</>
 	);
--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,29 +1,44 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import axios from "axios";
 import "./Home.css";
 const Home = () => {
 	const [fname, setFname] = useState("");
 	const [lname, setLname] = useState("");
-	const [fnameValue, setFnameValue] = useState("");
-	const [lnameValue, setLnameValue] = useState("");
-	const [res, setRes] = useState("");
-	// let firstName = document.getElementById("firstName");
-	// let secondName = document.getElementById("secondName");
-	useEffect(() => {
+	let navigate = useNavigate();
+	const getResult = (firstName, lastName) => {
 		const options = {
 			method: "GET",
+			url: "https://love-calculator.p.rapidapi.com/getPercentage",
+			params: { sname: fname, fname: lname },
 			headers: {
 				"X-RapidAPI-Key": "0359bd5187msh1d9d91398b35961p168041jsn1ba3b053ae5b",
 				"X-RapidAPI-Host": "love-calculator.p.rapidapi.com",
 			},
 		};
-		fetch(
-			`https://love-calculator.p.rapidapi.com/getPercentage?sname=${fname}&fname=${lname}`,
-			options
-		)
-			.then((response) => response.json())
-			.then((response) => setRes(response))
-			.catch((err) => console.error(err));
-	}, [fname, lname]);
+
+		axios
+			.request(options)
+			.then(function (response) {
+				if (response.status === 200) {
+					console.log(response);
+					console.log(response.data);
+					localStorage.setItem("loveResult", JSON.stringify(response.data));
+					navigate("/result");
+				} else {
+					Swal.fire({
+						icon: "error",
+						title: "Oops...",
+						text: "Something went wrong!",
+						footer: '<a href="">Why do I have this issue?</a>',
+					});
+				}
+			})
+			.catch(function (error) {
+				console.error(error);
+			});
+	};
 	return (
 		<div className="home">
 			<div className="container h-100 d-flex justify-content-center align-items-center flex-column">
@@ -31,32 +46,35 @@ const Home = () => {
 					className="w-100"
 					onSubmit={(e) => {
 						e.preventDefault();
-						setFname(fnameValue);
-						setLname(lnameValue);
+						console.log(fname);
+						console.log(lname);
+						getResult(fname, lname);
 					}}
 				>
 					<div className="row mt-5">
 						<div className="col-6">
 							<input
+								required
 								type="text"
 								placeholder="First Person Name"
 								className="form-control"
 								id="firstName"
 								aria-describedby="emailHelp"
 								onChange={(e) => {
-									setFnameValue(e.target.value);
+									setFname(e.target.value);
 								}}
 							></input>
 						</div>
 						<div className="col-6">
 							<input
+								required
 								type="text"
 								placeholder="Second Person Name"
 								className="form-control"
 								id="secondName"
 								aria-describedby="emailHelp"
 								onChange={(e) => {
-									setLnameValue(e.target.value);
+									setLname(e.target.value);
 								}}
 							></input>
 						</div>
@@ -66,7 +84,6 @@ const Home = () => {
 					</button>
 				</form>
 			</div>
-			<p>{res.result}</p>
 		</div>
 	);
 };
