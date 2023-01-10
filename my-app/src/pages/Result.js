3dfa9077f6452c9bbeb52567337af1e2549795f5
MaroMohamedSalah <91625271+MaroMohamedSalah@users.noreply.github.com>
2023-01-10 19:12:06 +0200

    done with result page

--- my-app/src/pages/Result.js
+++ my-app/src/pages/Result.js
@@ -1,11 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { atom, useRecoilState } from "recoil";
 import resultState from "../atoms/Result-atom";
 import "./Result.css";
 const Result = () => {
 	const [show, setShow] = useState(false);
+	const [showRes, setShowRes] = useState(false);
 	const [res, setRes] = useRecoilState(resultState);
-	console.log("hi", res);
+	setTimeout(() => {
+		setShow(true);
+		setShowRes(true);
+	}, 500);
 	return (
 		<div className="Home Result d-flex justify-content-center align-items-center">
 			<div className="box">
@@ -14,7 +18,7 @@ const Result = () => {
 						<div className="col-12 first">
 							<div className="center">
 								<i className="fa-regular fa-heart heart"></i>
-								<h4 onClick={() => setShow(true)}>30%</h4>
+								<h4 onClick={() => setShow(true)}>{res.percentage}%</h4>
 							</div>
 						</div>
 						<div className="col-6 names">
@@ -26,12 +30,13 @@ const Result = () => {
 										: { transform: "translateY(0px)" }
 								}
 							>
-								first name
+								{res.fname}
 							</span>
 							<i
 								className={`fa-solid fa-heart ${
 									show === false ? "opacity-0" : "opacity-1"
 								}`}
+								onClick={() => setShowRes(true)}
 							></i>
 							<span
 								className={show === false ? "opacity-0" : "opacity-1"}
@@ -41,15 +46,34 @@ const Result = () => {
 										: { transform: "translateY(0px)" }
 								}
 							>
-								second name
+								{res.sname}
 							</span>
 						</div>
 						<div className="col-6 res-description">
-							<p className="opacity-0">
-								Lorem, ipsum dolor sit amet consectetur
+							<p className={showRes === false ? "opacity-0" : "opacity-1"}>
+								{res.result}
 							</p>
-							<div className="facebook-share opacity-0">
-								<i className="fa-brands fa-facebook-f "></i>
+							<div
+								className={`facebook-share ${
+									showRes === false ? "opacity-0" : "opacity-1"
+								}`}
+							>
+								<i className="fa-brands fa-facebook-f"></i>
+							</div>
+							<div
+								class="fb-share-button"
+								data-href="https://developers.facebook.com/docs/plugins/"
+								data-layout="button_count"
+								data-size="small"
+							>
+								<a
+									target="_blank"
+									href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse"
+									class="fb-xfbml-parse-ignore"
+									rel="noreferrer"
+								>
+									Share
+								</a>
 							</div>
 						</div>
 					</div>
