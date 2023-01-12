f37d7108affaf575484fe4a3d776d3aacbf4d2d9
MaroMohamedSalah <91625271+MaroMohamedSalah@users.noreply.github.com>
2023-01-12 02:37:47 +0200

    add google ads sim

--- my-app/src/components/Google-ads.js
+++ my-app/src/components/Google-ads.js
@@ -2,7 +2,22 @@ import "./GoogleAds.css";
 const GoogleAds = () => {
 	return (
 		<div className="google-ads d-none d-md-block d-flex justify-content-center align-items-center">
-			<h1 className="text-center fs-6 text">Google ads</h1>
+			<h1 className="text-center fs-6 text-black-50">Advertisement</h1>
+			<button
+				type="button"
+				aria-label="Close"
+				onClick={() => {
+					document.getElementById("ads").style.backgroundImage = "none";
+				}}
+			>
+				<i className="fa-solid fa-xmark"></i>
+			</button>
+			<div
+				id="ads"
+				onClick={() => {
+					window.location.href = "https://maromohamedsalah.github.io/ehsebly/";
+				}}
+			></div>
 		</div>
 	);
 };
